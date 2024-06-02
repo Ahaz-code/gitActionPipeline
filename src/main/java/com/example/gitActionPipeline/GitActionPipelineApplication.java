ae3a1bbe1d3ed440aed4681f9c96591ee81b1f7c
yidnekachew <yidnekachew.esmail@gmail.com>
2024-06-02 10:37:56 +0300

    still testing

---
+++ src/main/java/com/example/gitActionPipeline/GitActionPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitActionPipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gitActionPipeline/GitActionPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitActionPipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
